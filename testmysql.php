dee7f54b2c501158111428ff6a2a30045c1d3aaf
valizr <valizr@gmail.com>
2015-12-10 02:11:42 +0200

    pus baza de date

---
+++ testmysql.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "restaurante-baiamare.ro";
+$username = "restabm_restaur";
+$password = "trinity";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?> 
