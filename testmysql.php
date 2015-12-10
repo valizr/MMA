dcbb824f8e2d5c1e6b9a7d686383c57093c0bb6d
valizr <valizr@gmail.com>
2015-12-10 02:19:07 +0200

    pus baza de date

--- testmysql.php
+++ testmysql.php
@@ -10,5 +10,6 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+//futai de baza de date
 echo "Connected successfully";
 ?> 
